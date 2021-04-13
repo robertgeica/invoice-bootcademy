67c8f286066ffe8bc2c8a15d5757fcfa5d866591
robertgeica <geicarobert@gmail.com>
2021-04-13 11:37:10 +0300

    save/get localStorage

--- tabel.js
+++ tabel.js
@@ -1,3 +1,80 @@
+let lsData = []; // array that holds localStorage data
+
+const saveToLocalStorage = () => {
+  const rows = document.querySelectorAll('.table-row');
+  let array = [];
+  
+  rows.forEach(row => {
+    
+    const obj = {
+      nrCrt: row.childNodes[0].innerHTML,
+      denumireProdus: row.childNodes[1].innerHTML,
+      um: row.childNodes[2].innerHTML,
+      cantitate: row.childNodes[3].innerHTML,
+      pretUnitar: row.childNodes[4].innerHTML,
+      valoareLei: row.childNodes[5].innerHTML,
+      valoareTVA: row.childNodes[6].innerHTML
+    };
+
+    array = [...array, obj];
+  });
+
+  localStorage.setItem('factura', JSON.stringify(array));
+};
+
+let isLoaded = false;
+const loadLocalStorage = () => {
+  // prevent duplicate data
+  if(isLoaded) return 'Data from localstorage was already loaded';
+
+ 
+  const localStorageData = localStorage.getItem('factura');
+  lsData = JSON.parse(localStorageData);
+
+  const table = document.getElementById("#invoice-table");  
+  if(localStorageData !== undefined) {
+
+    lsData.forEach(obj => {
+       // get rows length
+      const rows = document.querySelectorAll('.table-row');
+
+      const row = table.insertRow(nrCrt + 2);
+      row.className = 'table-row';
+
+      const cell1 = row.insertCell(0);
+      cell1.className = 'tabel-cell';
+
+      const cell2 = row.insertCell(1);
+      cell2.className = 'tabel-cell';
+      cell2.colSpan = 2;
+
+      const cell3 = row.insertCell(2);
+      cell3.className = 'tabel-cell';
+
+      const cell4 = row.insertCell(3);
+      cell4.className = 'tabel-cell';
+
+      const cell5 = row.insertCell(4);
+      cell5.className = 'tabel-cell';
+
+      const cell6 = row.insertCell(5);
+      cell6.className = 'tabel-cell';
+
+      const cell7 = row.insertCell(6);
+      cell7.className = 'tabel-cell';
+
+      cell1.innerHTML = rows.length == 0 ? obj.nrCrt : rows.length+1;
+      cell2.innerHTML = obj.denumireProdus;
+      cell3.innerHTML = obj.um;
+      cell4.innerHTML = obj.cantitate;
+      cell5.innerHTML = obj.pretUnitar;
+      cell6.innerHTML = obj.valoareLei;
+      cell7.innerHTML = obj.valoareTVA;
+    })
+  }
+  isLoaded = true;
+}
+
 const calculateTotalPrice = () => {
   const pricesList = document.querySelectorAll(".pret");
   let totalPrice = 0;
@@ -18,8 +95,10 @@ const calculateTotalPrice = () => {
 var nrCrt = 0;
 function addNewRow() {
 
-  var table = document.getElementById("#mytable");
+  var table = document.getElementById("#invoice-table");
   var row = table.insertRow(nrCrt + 2);
+  row.className = 'table-row';
+
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
@@ -30,56 +109,44 @@ function addNewRow() {
   var cell8 = row.insertCell(7);
 
   cell1.innerHTML = ++nrCrt;
-  cell1.style.border = "thin solid black";
-  cell1.style.textAlign = "center";
-  cell1.style.fontSize = "20px";
+  cell1.className = 'tabel-cell';
 
   var denumire = document.getElementById("produse-denumire").value;
   cell2.innerHTML = denumire;
-  cell2.style.border = "thin solid black";
+  cell2.className = 'tabel-cell';
   cell2.colSpan = "2";
-  cell2.style.textAlign = "center";
-  cell2.style.fontSize = "20px";
 
   var um = document.getElementById("um").value;
   cell3.innerHTML = um;
-  cell3.style.border = "thin solid black";
-  cell3.style.textAlign = "center";
-  cell3.style.fontSize = "20px";
+  cell3.className = 'tabel-cell';
 
   var cantitate = document.getElementById("produse-cantitate").value;
   cell4.innerHTML = cantitate;
-  cell4.style.border = "thin solid black";
-  cell4.style.textAlign = "center";
-  cell4.style.fontSize = "20px";
+  cell4.className = 'tabel-cell';
 
   var pret_unitar = document.getElementById("produse-pret-unitar").value;
   cell5.innerHTML = pret_unitar;
-  cell5.style.border = "thin solid black";
-  cell5.style.textAlign = "center";
-  cell5.style.fontSize = "20px";
+  cell5.className = 'tabel-cell';
 
-  cell6.style.border = "thin solid black";
-  cell6.style.textAlign = "center";
+  
   cell6.classList.add("pret");
-  cell6.style.fontSize = "20px";
+  cell6.className = 'tabel-cell';
   cell6.innerHTML = cantitate*pret_unitar;
 
   var valTVA =  0.1*cantitate*pret_unitar;
-  cell7.style.border = "thin solid black";
-  cell7.style.textAlign = "center";
-  cell7.style.fontSize = "20px";
+  cell7.className = 'tabel-cell';
   cell7.innerHTML = valTVA.toFixed(2);
 
-  // am creat butonul de stergere din dreapta fiecarei noi intrari pe care o adaugam cu ajutorul butonului "adauga"
   var button_stergere = document.createElement("button");
-  button_stergere.innerHTML = "X";
   cell8.appendChild(button_stergere);
   cell8.style.border = "0px none white";
-  button_stergere.style.backgroundColor = "#ff4d4d";
-  button_stergere.style.color = "#ffffff";
   cell8.style.width = "10px";
   cell8.className = "adauga-elemente";
+  
+  // am creat butonul de stergere din dreapta fiecarei noi intrari pe care o adaugam cu ajutorul butonului "adauga"
+  button_stergere.style.backgroundColor = "#ff4d4d";
+  button_stergere.style.color = "#ffffff";
+  button_stergere.innerHTML = "X";
   button_stergere.id = "#buton-de-stergere";
 
   //aici am facut ca, atunci cand apesi pe butonul "X" (cel creat deasupra), intrarea (randul) sa se stearga.
